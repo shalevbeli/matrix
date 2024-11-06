96c96ea8b6be575f6faeddc0299a9030fce15dc3
Shalev Zelinger <shalev.zelinger@gmail.com>
2024-11-06 19:10:35 +0200

    Chagned some minor details

--- Client/app.py
+++ Client/app.py
@@ -12,22 +12,28 @@ server_url = os.getenv("SERVER_URL", "http://localhost:5000/api")
 
 # Function to get a message from the server
 def get_message():
-    response = requests.get(f"{server_url}/message")
-    if response.status_code == 200:
-        print("Server message:", response.json()["message"])
-    else:
-        print("Failed to get message from server")
+    try:
+        response = requests.get(f"{server_url}/message")
+        if response.status_code == 200:
+            print("Server message:", response.json()["message"])
+        else:
+            print("Failed to get message from server")
+    except:
+        print("Client is unavailable, can't receieve message!")
 
 # Function to send data to the server and get processed data
 def send_data(input_data):
-    response = requests.post(f"{server_url}/process", json={"input_data": input_data})
-    if response.status_code == 200:
-        print("Processed data from server:", response.json()["processed_data"])
-    else:
-        print("Failed to process data on server")
+    try:    
+        response = requests.post(f"{server_url}/process", json={"input_data": input_data})
+        if response.status_code == 200:
+            print("Processed data from server:", response.json()["processed_data"])
+        else:
+            print("Failed to process data on server")
+    except:
+        print("Client is unavailable, can't send data!")
 
 if __name__ == "__main__":
     while True:
         get_message()
         send_data("hello world")
-        time.sleep("30")
+        time.sleep(30)
